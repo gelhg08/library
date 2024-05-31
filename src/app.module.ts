d6c7a3a6816d0d69a39771c8515b7c2ba8790b81
gelhg08 <angelicamhg31@gmail.com>
2024-05-31 11:37:19 -0500

    create connection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: true,
+    }),
+    BooksModule,
+  ],
   controllers: [],
   providers: [],
 })
